8b7fcbf2f675b627f5734209a5d147ef1b75e2f6
Zoro3435x <alejandro091963@gmail.com>
2025-04-29 12:54:18 -0600

    Proyecto Madereria

--- Madereria/asgi.py
+++ Madereria/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for madereriaapp project.
+ASGI config for Madereria project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'madereriaapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Madereria.settings')
 
 application = get_asgi_application()
---
+++ categoria_productos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CategoriaProductosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'categoria_productos'
